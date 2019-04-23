3bb64d8ea31e8b0a1c9fb10ca8f4969d0b2e5c20
Matthew Huk <matthew.huk@bglgroup.co.uk>
2019-04-23 16:10:05 +0100

    use variables for main

--- deployment/config.sh
+++ deployment/config.sh
@@ -13,6 +13,7 @@ rm -Rf ./config-file
 
 read -d ';' CONFIG_SETTINGS << EOL
 {
+  "MAIN_ACCOUNT": "${MAIN_ACCOUNT}",
   "ENVIRONMENT": "${ENVIRONMENT}",
   "NGINX_VERSION": "${NGINX_VERSION}",
   "JIRA_USER": "${JIRA_USER}",
@@ -24,6 +25,6 @@ read -d ';' CONFIG_SETTINGS << EOL
 };
 EOL
 
-aws --region eu-west-1 kms encrypt --key-id arn:aws:kms:eu-west-1:482506117024:alias/quoting --plaintext "${CONFIG_SETTINGS}" --query CiphertextBlob --output text | base64 --decode > config-file
+aws --region eu-west-1 kms encrypt --key-id arn:aws:kms:eu-west-1:${MAIN_ACCOUNT}:alias/quoting --plaintext "${CONFIG_SETTINGS}" --query CiphertextBlob --output text | base64 --decode > config-file
 
 aws s3 cp config-file s3://ctm-app-config/${PRODUCT}/${SERVICE_NAME}/${ENVIRONMENT}/config-settings --sse
